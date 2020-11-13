cf0ad3bf7578b54f5f0e9cdec3d53498e80ff6a3
Alessandro Degano <a.degano@gmail.com>
2020-11-13 15:12:08 +0100

    Transform to a daemon.

--- dyndns-updater.go
+++ dyndns-updater.go
@@ -1,32 +1,86 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/rs/zerolog/log"
 
 	"github.com/spf13/viper"
 )
 
-func main() {
+type config struct {
+	url  string
+	tick time.Duration
+}
+
+func (c *config) init() error {
 	viper.SetEnvPrefix("dyndns")
+
+	// Set the URL to probe from environment
 	viper.BindEnv("url")
 	url := viper.Get("url")
 	if url == nil {
 		log.Fatal().
 			Msg("The enviornment variable DYNDNS_URL must be set.")
 	}
+	c.url = url.(string)
 
-	log.Info().Msg("Querying Dyn-dns to update IP.")
-	resp, err := http.Get(url.(string))
-	if err != nil {
-		log.Fatal().Err(err).Msg("The query failed.")
+	// Set the probing interval, in seconds, from the environment
+	viper.BindEnv("interval")
+	interval := viper.Get("interval")
+	if interval == nil {
+		log.Fatal().Msg("The environment variable DYNDNS_INTERVAL must be set.")
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	tick, err := strconv.Atoi(interval.(string))
 	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to decode response of query.")
+		log.Fatal().Msgf("Cannot convert interval: %s to integer.", interval.(string))
 	}
-	log.Info().Msgf("Successful query, response: %s", body)
+	c.tick = time.Duration(tick) * time.Second
+
+	return nil
+}
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	c := &config{}
+
+	defer func() {
+		cancel()
+	}()
+
+	if err := run(ctx, c); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, c *config) error {
+	c.init()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-time.Tick(c.tick):
+			log.Info().Str("url", c.url).Msg("Querying.")
+			resp, err := http.Get(c.url)
+			if err != nil {
+				log.Warn().Err(err).Str("url", c.url).Msg("The query failed.")
+			}
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Warn().Err(err).Str("url", c.url).Msg("Failed to decode response of query")
+			}
+			log.Info().Str("url", c.url).Msgf("Successful query response: %s", body)
+		}
+	}
 }
